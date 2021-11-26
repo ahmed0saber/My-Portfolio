3e787a178ab206c37ce60a4ace73768ea31a45c3
Ahmed Saber <ahmed0saber33@gmail.com>
2021-11-27 01:53:24 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-/* EMPTY */
+var menu = document.querySelector('nav');
+var menuBtn = document.querySelector('nav button');
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('opened');
+});
+
+function send(){
+    /*https://api.whatsapp.com/send?phone=+201208611892&text=Hi*/
+    var name = document.getElementById("name").value;
+    var msg = document.getElementById("msg").value;
+    window.open("https://api.whatsapp.com/send?phone=+201208611892&text=Hi, Ahmed! My name is "+name+", I want to tell you that '"+msg+"'")
+}
